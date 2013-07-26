7604402110e5dfd7b21e30ddcbb6fd00ba5a6bcf
Craig Dunn <craig.dunn@conceptdevelopment.net>
2013-07-26 15:27:12 -0700

    [sample][android] implement Delete

--- SalesforceSample.Android/DetailActivity.cs
+++ SalesforceSample.Android/DetailActivity.cs
@@ -9,6 +9,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using System.Json;
+using Salesforce;
 
 namespace SalesforceSample.Droid
 {
@@ -34,6 +35,31 @@ namespace SalesforceSample.Droid
 		}
 
 
+		async void Delete () 
+		{
+			var selectedObject = new SObject (data as JsonObject);
+
+			// Delete the row from the data source.
+			var request = new DeleteRequest (selectedObject) {Resource = selectedObject};
+
+			await RootActivity.Client.ProcessAsync (request);
+		}
+
+		/// <summary>shortcut back to the main screen</summary>
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+			MenuInflater.Inflate (Resource.Menu.Delete, menu);
+			return true;
+		}
+		/// <summary>shortcut back to the main screen</summary>
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			if (item.ItemId == Resource.Id.delete) {
+				StartActivity (typeof(RootActivity));
+
+			}
+			return true;
+		}
 	}
 }
 
