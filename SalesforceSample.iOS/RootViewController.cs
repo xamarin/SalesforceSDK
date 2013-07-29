552b87fd471bc276a31479b86f211f2e9d2ae398
Jason Smith <jassmith@gmail.com>
2013-07-28 21:46:50 -0700

    Remove unneeded defaults

--- SalesforceSample.iOS/RootViewController.cs
+++ SalesforceSample.iOS/RootViewController.cs
@@ -16,31 +16,31 @@ namespace SalesforceSample.iOS
 
 		public string Name
 		{
-			get { return GetOption ("Name", string.Empty); }
+			get { return GetOption ("Name"); }
 			set { SetOption ("Name", value); }
 		}
 
 		public string Phone
 		{
-			get { return GetOption ("Phone", string.Empty); }
+			get { return GetOption ("Phone"); }
 			set { SetOption ("Phone", value); }
 		}
 
 		public string Industry
 		{
-			get { return GetOption ("Industry", string.Empty); }
+			get { return GetOption ("Industry"); }
 			set { SetOption ("Industry", value); }
 		}
 
 		public string Website
 		{
-			get { return GetOption ("Website", string.Empty); }
+			get { return GetOption ("Website"); }
 			set { SetOption ("Website", value); }
 		}
 
 		public string AccountNumber
 		{
-			get { return GetOption ("AccountNumber", string.Empty); }
+			get { return GetOption ("AccountNumber"); }
 			set { SetOption ("AccountNumber", value); }
 		}
 	}
