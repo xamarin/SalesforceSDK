48e8117e968717c771ca424728ee45a72a7b7405
Jason Smith <jassmith@gmail.com>
2013-07-29 14:36:26 -0700

    Cleanup android sample

--- SalesforceSample.Android/DetailAdapter.cs
+++ SalesforceSample.Android/DetailAdapter.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using System.Json;
@@ -14,49 +8,34 @@ namespace SalesforceSample.Droid
 {
 	public class DetailAdapter : BaseAdapter<Tuple<string,string>>
 	{
-		Activity context;
-		JsonValue data;
+		readonly Activity context;
 
-		public DetailAdapter(Activity activity, JsonValue item)
-			: base()
+		public DetailAdapter(Activity activity, AccountObject item)
 		{
 			context = activity;
-			data = item;
+			Data = item;
 		}
 
-		public JsonValue Data
-		{
-			get { return data; }
-			set
-			{
-				if (data == value)
-					return;
-				data = value;
-				// refresh ((ListActivity)context).ListView.
-			}
-		}
+		AccountObject Data { get; set; }
 
 		public override long GetItemId(int position)
 		{
 			return position;
 		}
+		
 		public override Tuple<string,string> this[int position]
 		{
-			get { 
-				//var key = 
-				return new Tuple<string,string> ("",""); }//datapairs[position]; 
+			get { return new Tuple<string,string> ("",""); }
 		}
+		
 		public override int Count
 		{
-			get { return data.Count; }
+			get { return 5; }
 		}
+
 		public override View GetView(int position, View convertView, ViewGroup parent)
 		{
-			Data = (JsonObject)data;
-
-			View view = convertView;
-			if (view == null) // no view to re-use, create new
-				view = context.LayoutInflater.Inflate(Resource.Layout.DetailListViewCell, null);
+			var view = convertView ?? context.LayoutInflater.Inflate(Resource.Layout.DetailListViewCell, null);
 
 			// clear field
 			view.FindViewById<TextView>(Resource.Id.Text1).Text = "";
@@ -65,23 +44,23 @@ namespace SalesforceSample.Droid
 			switch (position) {
 				case 0:
 				view.FindViewById<TextView>(Resource.Id.Text1).Text = "Name";
-				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data["Name"];
+				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data.Name;
 				break;
 				case 1:
 				view.FindViewById<TextView>(Resource.Id.Text1).Text = "Industry";
-				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data["Industry"];
+				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data.Industry;
 				break;
 				case 2:
 				view.FindViewById<TextView>(Resource.Id.Text1).Text = "Phone";
-				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data["Phone"];
+				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data.Phone;
 				break;
 				case 3:
 				view.FindViewById<TextView>(Resource.Id.Text1).Text = "Website";
-				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data["Website"];
+				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data.Website;
 				break;
 				case 4:
 				view.FindViewById<TextView>(Resource.Id.Text1).Text = "Account Number";
-				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data["AccountNumber"];
+				view.FindViewById<TextView>(Resource.Id.Text2).Text =  Data.AccountNumber;
 				break;
 			}
 
